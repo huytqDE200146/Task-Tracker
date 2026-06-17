b93d3333c57dd5d6ff431a43a4a2bb9fc38c4a25
huytqDE200146 <huytqde200146@gmail.com>
2026-06-17 17:50:07 +0700

    add About.js

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h2>About</h2>
+      <p>Task Tracker là project cá nhân cho môn FER202, xây dựng bằng React, Bootstrap và Redux.</p>
+    </div>
+  );
+};
+
+export default About;
