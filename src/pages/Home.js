520b78811d5abb811f789c37bc93c13fc03b0ccc
huytqDE200146 <huytqde200146@gmail.com>
2026-06-17 17:47:58 +0700

    add Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Welcome to Task Tracker</h2>
+      <p>Quản lý công việc cá nhân một cách đơn giản và hiệu quả.</p>
+    </div>
+  );
+};
+
+export default Home;
