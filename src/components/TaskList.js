e4964d0592fd2e6c341fb4673d7e65709837686e
huytqDE200146 <huytqde200146@gmail.com>
2026-06-17 18:05:26 +0700

    update TaskList.js

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -2,12 +2,16 @@ import React from 'react';
 import TaskItemFunctional from './TaskItemFunctional';
 
 // Arrow function component
-const TaskList = ({ tasks }) => {
+const TaskList = ({ tasks, onToggleStatus, onDelete }) => {
   return (
     <div className="task-list">
       {tasks.map(({ id, ...rest }) => (
-        // destructuring task object, template literal cho key
-        <TaskItemFunctional key={`task-${id}`} task={{ id, ...rest }} />
+        <TaskItemFunctional
+          key={`task-${id}`}
+          task={{ id, ...rest }}
+          onToggleStatus={onToggleStatus}
+          onDelete={onDelete}
+        />
       ))}
     </div>
   );
